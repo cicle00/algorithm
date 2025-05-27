02f2e22b3447e25fc5ec3ec43baea7b1623d94d1
cicle00 <cicle1999@gmail.com>
2025-05-27 13:03:27 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] T = new int[N + 1]; 
+        int[] P = new int[N + 1]; 
+        int[] dp = new int[N + 2]; 
+
+        for (int i=1;i<=N;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            T[i] = Integer.parseInt(st.nextToken());
+            P[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=N;i>=1;i--){
+            int time = i + T[i];
+            
+            if(time<=N+1) dp[i] = Math.max(dp[i+1], P[i]+dp[time]);
+            else dp[i] = dp[i+1];
+        }
+        
+        System.out.println(dp[1]);
+        
+    }
+}
