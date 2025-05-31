790a044ee4f25c26e0dc8880d30e5af04da17981
cicle00 <cicle1999@gmail.com>
2025-05-31 14:15:12 +0900

    [Silver IV] Title: 로프, Time: 304 ms, Memory: 26476 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] rope = new int[N];
+        
+        for(int i=0;i<N;i++) {
+        	rope[i] = Integer.parseInt(br.readLine());
+        }
+        
+        Arrays.sort(rope);
+        
+        int max = 0;
+        for(int i=0; i<N; i++) {
+            int weight = rope[i] * (N - i); 
+            if (weight > max) max = weight;
+        }
+        
+        System.out.println(max);
+
+    }
+}
