b7682509408ba8165bfa9091b601a81794b21165
cicle00 <cicle1999@gmail.com>
2025-06-13 00:04:02 +0900

    [Silver IV] Title: 암기왕, Time: 1492 ms, Memory: 285036 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -4,41 +4,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder(); 
-
+        StringBuilder sb = new StringBuilder();
+        
         int T = Integer.parseInt(br.readLine());
+        
+        for(int t=0;t<T;t++) {
+        int N = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Set<Integer> note1 = new HashSet<>();
+        for(int i=0;i<N;i++) {
+        	note1.add(Integer.parseInt(st.nextToken()));
+        }
+       
+        
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        while (T-- > 0) {
-            int N = Integer.parseInt(br.readLine());
-            int[] note1 = new int[N];
-
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) {
-                note1[i] = Integer.parseInt(st.nextToken());
-            }
-
-            Arrays.sort(note1);
-
-            int M = Integer.parseInt(br.readLine());
-            st = new StringTokenizer(br.readLine());
-
-            for (int i = 0; i < M; i++) {
-                int target = Integer.parseInt(st.nextToken());
-                sb.append(binarySearch(note1, target)).append("\n"); 
-            }
+        for(int i=0;i<M;i++) {
+        	int note2 = Integer.parseInt(st.nextToken());
+        	int exist = 0;
+        	if(note1.contains(note2)) exist = 1;
+        	sb.append(exist).append("\n");
         }
 
-        System.out.print(sb); 
-    }
-
-    static int binarySearch(int[] arr, int target) {
-        int left = 0, right = arr.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (arr[mid] == target) return 1;
-            else if (arr[mid] < target) left = mid + 1;
-            else right = mid - 1;
         }
-        return 0;
-    }
+        System.out.println(sb);
+	}
 }
