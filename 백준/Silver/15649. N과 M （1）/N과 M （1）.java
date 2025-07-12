97403bc67528e7ec3e4e65ebfad68b4506540bb1
cicle00 <cicle1999@gmail.com>
2025-07-12 22:01:50 +0900

    [Silver III] Title: N과 M (1), Time: 2064 ms, Memory: 76676 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static boolean[] visited;
+    static int[] result;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());  
+        M = Integer.parseInt(st.nextToken());  
+
+        visited = new boolean[N + 1];  
+        result = new int[M];          
+
+        dfs(0);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                System.out.print(result[i] + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;   
+                result[depth] = i;     
+                dfs(depth + 1);      
+                visited[i] = false;     
+            }
+        }
+    }
+}
