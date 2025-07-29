33ea4e646822eac239bca714bbfff8f7b95f66b1
cicle00 <cicle1999@gmail.com>
2025-07-29 23:36:33 +0900

    [Silver IV] Title: 빙고, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -1,75 +1,90 @@
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int[][] board = new int[5][5];
-        boolean[][] marked = new boolean[5][5];
-        Map<Integer, int[]> positionMap = new HashMap<>();
+    static int[][] nums = new int[5][5];
+    static boolean[][] check = new boolean[5][5];
+    static boolean bingo = false;
+    static int count = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        // 빙고판 입력 및 위치 매핑
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                int num = sc.nextInt();
-                board[i][j] = num;
-                positionMap.put(num, new int[]{i, j});
+        for(int i=0;i<5;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0;j<5;j++){
+                nums[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
         int count = 0;
         boolean bingoAchieved = false;
 
-        // 사회자가 부르는 숫자 처리
         for (int i = 0; i < 5 && !bingoAchieved; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 5 && !bingoAchieved; j++) {
-                int calledNum = sc.nextInt();
+                int num = Integer.parseInt(st.nextToken());
+                numCheck(num);
                 count++;
-                int[] pos = positionMap.get(calledNum);
-                marked[pos[0]][pos[1]] = true;
-
-                if (count >= 12 && checkBingo(marked) >= 3) {
-                    System.out.println(count);
+                if(isBingo()>=3) {
                     bingoAchieved = true;
+                    break;
                 }
             }
+            if(bingoAchieved) break;
         }
-        sc.close();
+
+        System.out.println(count);
+
     }
 
-    // 빙고 줄 수 계산
-    static int checkBingo(boolean[][] marked) {
+    static void numCheck(int n){
+        for(int i=0;i<5;i++){
+            for(int j=0;j<5;j++){
+                if(nums[i][j]==n){
+                    check[i][j] = true;
+                }
+            }
+        }
+    }
+
+    static int isBingo(){
         int bingoCount = 0;
-
-        // 가로 체크
-        for (int i = 0; i < 5; i++) {
-            boolean rowComplete = true;
-            for (int j = 0; j < 5; j++) {
-                if (!marked[i][j]) {
-                    rowComplete = false;
+        //가로
+        for(int i=0;i<5;i++){
+            boolean rowBingo = true;
+            for(int j=0;j<5;j++){
+                if(!check[i][j]) {
+                    rowBingo = false;
                     break;
                 }
             }
-            if (rowComplete) bingoCount++;
+            if (rowBingo) bingoCount++;
         }
 
-        // 세로 체크
-        for (int j = 0; j < 5; j++) {
-            boolean colComplete = true;
-            for (int i = 0; i < 5; i++) {
-                if (!marked[i][j]) {
-                    colComplete = false;
+        //세로
+        for(int j=0;j<5;j++){
+            boolean colBingo = true;
+            for(int i=0;i<5;i++){
+                if(!check[i][j]) {
+                    colBingo = false;
                     break;
                 }
             }
-            if (colComplete) bingoCount++;
+            if (colBingo) bingoCount++;
         }
 
-        // 대각선 체크
+        //대각선
         boolean diag1 = true;
         boolean diag2 = true;
-        for (int i = 0; i < 5; i++) {
-            if (!marked[i][i]) diag1 = false;
-            if (!marked[i][4 - i]) diag2 = false;
+        for(int i=0;i<5;i++) {
+            if (!check[i][i]) {
+                diag1 = false;
+            }
+            if(!check[i][4-i]){
+                diag2 = false;
+            }
         }
         if (diag1) bingoCount++;
         if (diag2) bingoCount++;
