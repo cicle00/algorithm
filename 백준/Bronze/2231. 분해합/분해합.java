a41e44067c041264e1a99cb20963b8159cd2d7b2
cicle00 <cicle1999@gmail.com>
2025-08-01 19:43:19 +0900

    [Bronze II] Title: 분해합, Time: 168 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -1,26 +1,27 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
-        int result = 0;
+        int min = 0;
 
-        // M은 최소 N - 9 * 자릿수 ~ N 사이 범위만 확인하면 됨
-        for (int i = Math.max(1, N - 9 * String.valueOf(N).length()); i < N; i++) {
-            int sum = i;
+        for(int i = Math.max(1, N - 9 * String.valueOf(N).length());i<N;i++){
+            int n = i;
             int temp = i;
-            while (temp > 0) {
-                sum += temp % 10;
-                temp /= 10;
+            while(temp>0){
+                n += temp%10;
+                temp/=10;
             }
 
-            if (sum == N) {
-                result = i;
-                break; // 가장 작은 M을 찾으면 바로 종료
+            if(n == N) {
+                min = i;
+                break;
             }
         }
 
-        System.out.println(result);
+        System.out.println(min);
+
     }
 }
